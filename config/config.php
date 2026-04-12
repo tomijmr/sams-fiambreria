280b9bd3077c6f32d3e70da0cd2d576cb0bd0697
tomi <tms1csgo@gmail.com>
2026-04-12 20:53:20 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -30,10 +30,10 @@ define('BASE_URL', $baseUrl);
 $assetsUrl = str_ends_with(BASE_URL, '/public') ? (BASE_URL . '/assets') : (BASE_URL . '/public/assets');
 define('ASSETS_URL', rtrim($assetsUrl, '/'));
 
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'sams_kiosko');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_NAME', getenv('DB_NAME') ?: 'a0011086_sams');
+define('DB_USER', getenv('DB_USER') ?: 'a0011086');
+define('DB_PASS', getenv('DB_PASS') ?: 'PObitovi56');
 define('DB_CHARSET', 'utf8mb4');
 
 date_default_timezone_set('America/Argentina/Buenos_Aires');
