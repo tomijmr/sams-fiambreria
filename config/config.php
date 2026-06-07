d8a6aa4679a56ed3f9282bf440290da3f99391d9
tomi <tms1csgo@gmail.com>
2026-06-07 10:53:41 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -30,10 +30,16 @@ define('BASE_URL', $baseUrl);
 $assetsUrl = str_ends_with(BASE_URL, '/public') ? (BASE_URL . '/assets') : (BASE_URL . '/public/assets');
 define('ASSETS_URL', rtrim($assetsUrl, '/'));
 
+// define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+// define('DB_NAME', getenv('DB_NAME') ?: 'a0011086_sams');
+// define('DB_USER', getenv('DB_USER') ?: 'a0011086');
+// define('DB_PASS', getenv('DB_PASS') ?: 'PObitovi56');
+// define('DB_CHARSET', 'utf8mb4');
+
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-define('DB_NAME', getenv('DB_NAME') ?: 'a0011086_sams');
-define('DB_USER', getenv('DB_USER') ?: 'a0011086');
-define('DB_PASS', getenv('DB_PASS') ?: 'PObitovi56');
+define('DB_NAME', getenv('DB_NAME') ?: 'c2632136_gaucho');
+define('DB_USER', getenv('DB_USER') ?: 'c2632136_gaucho');
+define('DB_PASS', getenv('DB_PASS') ?: 'pobe44KOfo');
 define('DB_CHARSET', 'utf8mb4');
 
 date_default_timezone_set('America/Argentina/Buenos_Aires');
